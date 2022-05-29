72fe3ab5f728e8f5c23a4457678860ac5dcf6fbe
alessio <alessiovatti@gmail.com>
2022-05-29 21:55:32 +0200

    new lib for shmem communication

--- src/mylib.h
+++ src/mylib.h
@@ -1,3 +1,5 @@
-int get_number();
-
-
+#define ByteSize 512
+#define BackingFile "/shMemEx"
+#define AccessPerms 0644
+#define SemaphoreName "mysemaphore"
+#define MemContents "This is the way the world ends...\n"
